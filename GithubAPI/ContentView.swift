da6ce59edef688a525e22bd2972d6e9684e9584c
sumi <ninzin60en.dev@gmail.com>
2021-06-13 14:43:05 +0900

    Initial Commit

---
+++ GithubAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubAPI
+//
+//  Created by nekuro on 2021/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubAPI/GithubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAPIApp.swift
+//  GithubAPI
+//
+//  Created by nekuro on 2021/06/13.
+//
+
+import SwiftUI
+
+@main
+struct GithubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
